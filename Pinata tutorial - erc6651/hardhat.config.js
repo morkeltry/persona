395fca36e0b373186462bd26cc343de41fbdf20f
Riki0923 <83667276+Riki0923@users.noreply.github.com>
2023-11-17 23:32:35 +0100

    base 6651

---
+++ Pinata tutorial - erc6651/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+
+module.exports = {
+  solidity: "0.8.22",
+  networks: {
+		//Add extra chains as needed 
+    hardhat: {
+      chainId: 1337,
+    },
+      goerli: {
+        url: `${process.env.ALCHEMY_GOERLI_URL}`,
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      }, 
+  }, 
+  defaultNetwork: "hardhat",
+};
