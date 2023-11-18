9f11a16d17b50581933b292ec4a7fe6c81e764e9
azul <1337+morkeltry@users.noreply.github.com>
2023-11-18 10:16:45 +0300

    added scripts from pinata tut - still no entry point apparent 🤔

---
+++ packages/hardhat/pinata_tut_scripts/createAccount.js
@@ -0,0 +1,33 @@
+require("hardhat");
+require("dotenv").config();
+
+
+async function main() {
+  const Registry = await ethers.getContractFactory("ERC6551Registry");
+  const registry = await Registry.attach(process.env.ERC6551REGISTRY_ADDRESS);
+  //update salt for a more secure hash
+  const salt = 0;  
+  const implementation = process.env.ERC6551ACOUNT_ADDRESS
+  const tokenAddress = process.env.PINNIE_ADDRESS;
+  //replace with tokenId minted in scripts/mint.js, logged on the CLI
+  const tokenId = 0
+  const chainID = 5 //goerli
+  const initData = "0x";
+
+  const tx = await registry.createAccount(implementation, chainID, tokenAddress, tokenId, salt, initData);
+  const receipt = await tx.wait();
+  const address = await registry.account(implementation, chainID, tokenAddress, tokenId, salt)
+  
+  if(receipt.status == 1 && address){
+   console.log("Account created successfully at address: ", address);
+  }
+   else{
+    console.log("Account creation failed");
+  }
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
---
+++ packages/hardhat/pinata_tut_scripts/deploy.js
@@ -0,0 +1,24 @@
+require("hardhat");
+
+async function main() {
+  const Pinnie = await ethers.deployContract("Pinnie");
+  const pinnie = await Pinnie.waitForDeployment();
+ 
+
+  const Account = await ethers.deployContract("ERC6551Account");
+  const account = await Account.waitForDeployment();
+
+  const Registry = await ethers.deployContract("ERC6551Registry");
+  const registry = await Registry.waitForDeployment();
+
+  console.log("Pinnie contract deployed at:", pinnie.target);
+  console.log("Account contract deployed at:", account.target);
+  console.log("Registry contract deployed at:", registry.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
