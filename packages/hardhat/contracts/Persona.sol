2a452e94fcab565f5b582c583c2df867f299d378
Riki0923 <83667276+Riki0923@users.noreply.github.com>
2023-11-18 18:22:52 +0100

    Update and rename Pinnie.sol to Persona.sol

--- packages/hardhat/contracts/Persona.sol
+++ packages/hardhat/contracts/Persona.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Pinnie is ERC721, ERC721URIStorage, Ownable {
+contract Persona is ERC721, ERC721URIStorage, Ownable {
     uint256 public _tokenId;
     string private _baseTokenURI;
 
