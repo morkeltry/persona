f84a77046d460d76fcb6d3141e9b37424bff07f5
azul <1337+morkeltry@users.noreply.github.com>
2023-11-18 19:55:16 +0300

    add hardhat.config.js with multibaas entries

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+const {
+  ALCHEMY_GOERLI_URL,
+  MULTIBAAS_API_KEY,
+  MULTIBAAS_DEPLOYMENT_URL,
+  PRIVATE_KEY
+} = process.env;
 
 module.exports = {
   solidity: "0.8.22",
@@ -10,9 +16,26 @@ module.exports = {
       chainId: 1337,
     },
       goerli: {
-        url: `${process.env.ALCHEMY_GOERLI_URL}`,
-        accounts: [`0x${process.env.PRIVATE_KEY}`],
+        url: `${ALCHEMY_GOERLI_URL}`,
+        accounts: [`0x${PRIVATE_KEY}`],
       }, 
+      // multibaas default to multibaas_base_goerli
+      multibaas: {
+        url: `{MULTIBAAS_DEPLOYMENT_URL}/web3/{MULTIBAAS_API_KEY}`,
+        chainId: `84531`,
+        accounts: [`0x${PRIVATE_KEY}`],
+      },
+      multibaas_base_goerli: {
+        url: `{MULTIBAAS_DEPLOYMENT_URL}/web3/{MULTIBAAS_API_KEY}`,
+        chainId: `84531`,
+        accounts: [`0x${PRIVATE_KEY}`],
+      },
   }, 
   defaultNetwork: "hardhat",
+  mbConfig: {
+    apiKey: MULTIBAAS_API_KEY,
+    host: new URL(  MULTIBAAS_DEPLOYMENT_URL),
+    allowUpdateAddress: ["development"],
+    allowUpdateContract: ["development"],
+  },,
 };
