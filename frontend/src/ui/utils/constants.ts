6f95a7bc0d2f4ea22b3c1b2bcdd9819f6641b4a2
daniel adebimpe <daniel.adebimpe@ey.com>
2023-11-18 14:50:28 +0300

    ui

---
+++ frontend/src/ui/utils/constants.ts
@@ -0,0 +1,72 @@
+export enum PROPOSAL_VOTES {
+    NO_VOTE = 0,
+    VOTE_YES = 1,
+    VOTE_NO = 2,
+  }
+  export const communityCategories = [
+    { label: "All Categories", key: "all" },
+    { label: "Most Recent", key: "recent" },
+    { label: "Popular", key: "popular" },
+    { label: "Created by me", key: "me" },
+  ];
+  export const communitySizes = [
+    { label: "All Sizes", key: "all" },
+    { label: "0 - 20 Members", key: "0-20" },
+    { label: "21 - 50 Members", key: "21-50" },
+    { label: "51 - 100 Members", key: "51-100" },
+    { label: "101 - 500 Members", key: "101-500" },
+    { label: "+501 Members", key: "+501" },
+  ];
+  export const personaCategoryMenu = [
+    {
+      label: "All Category",
+      key: "all",
+    },
+    {
+      label: "CryptoPunks",
+      key: "cryptoPunks",
+    },
+    {
+      label: "Pudgy Penguins",
+      key: "pudgyPenguins",
+    },
+    {
+      label: "Art Blocks",
+      key: "artBlocks",
+    },
+    {
+      label: "Loot",
+      key: "loot",
+    },
+    {
+      label: "World of Women (WoW)",
+      key: "worldOfWomen",
+    },
+  ];
+  export const personaGuildMenu = [
+    {
+      label: "All Types",
+      key: "all",
+    },
+    {
+      label: "Engineers",
+      key: "engineer",
+    },
+    {
+      label: "Creators",
+      key: "creator",
+    },
+    {
+      label: "Writers",
+      key: "writer",
+    },
+    {
+      label: "Artists",
+      key: "artist",
+    },
+    {
+      label: "Investors",
+      key: "investor",
+    },
+  ];
+  
